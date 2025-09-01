4a0a76bbf9be3ddf2caa0247709169ccdbdd8420
Ben Jarvis <ben.jarvis@quantum.com>
2025-09-01 12:23:26 -0500

    Add syntax linting to CI
    * Add makefile targets to run uncrustify
    * Add github action to do a syntax check during CI. 
    * Fix some existing deviations from the uncrustify standard. (#16)

--- src/server/s3/s3_dump.h
+++ src/server/s3/s3_dump.h
@@ -7,8 +7,10 @@
 #include "s3_internal.h"
 
 void
-chimera_s3_dump_request(struct chimera_s3_request *request);
+chimera_s3_dump_request(
+    struct chimera_s3_request *request);
 
 void
-chimera_s3_dump_response(struct chimera_s3_request *request);
+chimera_s3_dump_response(
+    struct chimera_s3_request *request);
 
