9a1144781d4cd656cbf017f9f5a9a1e1769f5e6e
Ben Jarvis <ben.jarvis@quantum.com>
2025-12-23 14:52:51 +0000

    Avoid some code duplication and add missing portmap services.

--- src/server/nfs/nfs_portmap.c
+++ src/server/nfs/nfs_portmap.c
@@ -10,6 +10,31 @@
 #include "nfs_common.h"
 
 /*
+ * Service registration table for portmap/rpcbind responses.
+ * Lists all RPC services we advertise.
+ */
+struct portmap_service {
+    uint32_t prog;    /* RPC program number */
+    uint32_t vers;    /* Program version */
+    uint32_t prot;    /* Protocol: 6 = TCP, 17 = UDP */
+    uint32_t port;    /* Port number */
+};
+
+static const struct portmap_service portmap_services[] = {
+    /* Portmap/rpcbind - program 100000 */
+    { 100000, 2, 6, 111   },
+    { 100000, 3, 6, 111   },
+    { 100000, 4, 6, 111   },
+    /* NFS - program 100003 */
+    { 100003, 3, 6, 2049  },
+    { 100003, 4, 6, 2049  },
+    /* Mount - program 100005 */
+    { 100005, 3, 6, 20048 },
+};
+
+#define NUM_PORTMAP_SERVICES (sizeof(portmap_services) / sizeof(portmap_services[0]))
+
+/*
  * Convert local address from "ip:port" format to universal address format.
  * Universal address format for IPv4: "a.b.c.d.port_hi.port_lo"
  * where port = port_hi * 256 + port_lo
@@ -44,6 +69,103 @@ portmap_make_uaddr(
                     local_addr, port >> 8, port & 0xff);
 }
 
+/*
+ * Look up port number for a given program.
+ * Returns the port number, or 0 if not found.
+ */
+static unsigned int
+portmap_lookup_port(uint32_t prog)
+{
+    for (unsigned int i = 0; i < NUM_PORTMAP_SERVICES; i++) {
+        if (portmap_services[i].prog == prog) {
+            return portmap_services[i].port;
+        }
+    }
+    return 0;
+}
+
+/*
+ * Build a V2 pmaplist linked list from the service table.
+ * Allocates structures from the RPC message dbuf.
+ */
+static struct pmaplist *
+portmap_build_pmaplist(struct evpl_rpc2_msg *msg)
+{
+    struct pmaplist *head = NULL;
+    struct pmaplist *tail = NULL;
+    struct pmaplist *entry;
+
+    for (unsigned int i = 0; i < NUM_PORTMAP_SERVICES; i++) {
+        entry = xdr_dbuf_alloc_space(sizeof(*entry), msg->dbuf);
+        if (!entry) {
+            return head;
+        }
+
+        entry->map.prog = portmap_services[i].prog;
+        entry->map.vers = portmap_services[i].vers;
+        entry->map.prot = portmap_services[i].prot;
+        entry->map.port = portmap_services[i].port;
+        entry->next     = NULL;
+
+        if (tail) {
+            tail->next = entry;
+        } else {
+            head = entry;
+        }
+        tail = entry;
+    }
+
+    return head;
+}
+
+/*
+ * Build a V3/V4 rp__list linked list from the service table.
+ * Allocates structures and uaddr strings from the RPC message dbuf.
+ */
+static struct rp__list *
+portmap_build_rpcblist(
+    struct evpl_rpc2_conn *conn,
+    struct evpl_rpc2_msg  *msg)
+{
+    struct rp__list *head = NULL;
+    struct rp__list *tail = NULL;
+    struct rp__list *entry;
+    char            *uaddr;
+
+    for (unsigned int i = 0; i < NUM_PORTMAP_SERVICES; i++) {
+        entry = xdr_dbuf_alloc_space(sizeof(*entry), msg->dbuf);
+        if (!entry) {
+            return head;
+        }
+
+        uaddr = xdr_dbuf_alloc_space(64, msg->dbuf);
+        if (!uaddr) {
+            return head;
+        }
+
+        entry->rpcb_map.r_prog      = portmap_services[i].prog;
+        entry->rpcb_map.r_vers      = portmap_services[i].vers;
+        entry->rpcb_map.r_netid.str = "tcp";
+        entry->rpcb_map.r_netid.len = 3;
+        entry->rpcb_map.r_addr.len  = portmap_make_uaddr(conn,
+                                                         portmap_services[i].port,
+                                                         uaddr, 64);
+        entry->rpcb_map.r_addr.str  = uaddr;
+        entry->rpcb_map.r_owner.str = "";
+        entry->rpcb_map.r_owner.len = 0;
+        entry->next                 = NULL;
+
+        if (tail) {
+            tail->next = entry;
+        } else {
+            head = entry;
+        }
+        tail = entry;
+    }
+
+    return head;
+}
+
 void
 chimera_portmap_null_v2(
     struct evpl           *evpl,
@@ -72,22 +194,15 @@ chimera_portmap_getport_v2(
     unsigned int                      port;
     int                               rc;
 
-    switch (mapping->prog) {
-        case 100003:
-            port = 2049;
-            break;
-        case 100005:
-            port = 20048;
-            break;
-        default:
-            chimera_nfs_error("portmap request for unknown program %u",
-                              mapping->prog);
-            port = 0;
-    } /* switch */
+    port = portmap_lookup_port(mapping->prog);
+
+    if (port == 0) {
+        chimera_nfs_error("portmap request for unknown program %u",
+                          mapping->prog);
+    }
 
     rc = shared->portmap_v2.send_reply_PMAPPROC_GETPORT(evpl, port, msg);
     chimera_nfs_abort_if(rc, "Failed to send RPC2 reply");
-
 } /* chimera_portmap_getport */
 
 void
@@ -99,24 +214,47 @@ chimera_portmap_dump_v2(
 {
     struct chimera_server_nfs_thread *thread = private_data;
     struct chimera_server_nfs_shared *shared = thread->shared;
+    struct pmaplist                  *list;
     int                               rc;
-    struct pmaplist                   pmap_mountd, pmap_nfsd;
 
-    pmap_mountd.map.prog = 100005;
-    pmap_mountd.map.vers = 3;
-    pmap_mountd.map.prot = 6;
-    pmap_mountd.map.port = 20048;
-    pmap_mountd.next     = &pmap_nfsd;
+    list = portmap_build_pmaplist(msg);
 
-    pmap_nfsd.map.prog = 100003;
-    pmap_nfsd.map.vers = 3;
-    pmap_nfsd.map.prot = 6;
-    pmap_nfsd.map.port = 2049;
-    pmap_nfsd.next     = NULL;
-
-    rc = shared->portmap_v2.send_reply_PMAPPROC_DUMP(evpl, &pmap_mountd, msg);
+    rc = shared->portmap_v2.send_reply_PMAPPROC_DUMP(evpl, list, msg);
     chimera_nfs_abort_if(rc, "Failed to send RPC2 reply");
 } /* chimera_portmap_dump */
+
+/*
+ * Common implementation for V3/V4 GETADDR.
+ * Returns universal address for known programs.
+ */
+static void
+portmap_getaddr_common(
+    struct evpl           *evpl,
+    struct evpl_rpc2_conn *conn,
+    struct rpcb           *args,
+    struct evpl_rpc2_msg  *msg,
+    int                  (*send_reply)(struct evpl *, xdr_string *, void *))
+{
+    xdr_string   addr;
+    char         uaddr[64];
+    unsigned int port;
+    int          rc;
+
+    port = portmap_lookup_port(args->r_prog);
+
+    if (port == 0) {
+        chimera_nfs_error("rpcbind getaddr request for unknown program %u",
+                          args->r_prog);
+        addr.str = "";
+        addr.len = 0;
+    } else {
+        addr.len = portmap_make_uaddr(conn, port, uaddr, sizeof(uaddr));
+        addr.str = uaddr;
+    }
+
+    rc = send_reply(evpl, &addr, msg);
+    chimera_nfs_abort_if(rc, "Failed to send RPC2 reply");
+}
 
 void
 chimera_portmap_getaddr_v3(
@@ -128,37 +266,30 @@ chimera_portmap_getaddr_v3(
 {
     struct chimera_server_nfs_thread *thread = private_data;
     struct chimera_server_nfs_shared *shared = thread->shared;
-    xdr_string                        addr;
-    char                              uaddr[64];
-    unsigned int                      port;
-    int                               rc;
 
-    /* Return universal address for known programs */
-    switch (args->r_prog) {
-        case 100000: /* PORTMAP */
-            port = 111;
-            break;
-        case 100003: /* NFS */
-            port = 2049;
-            break;
-        case 100005: /* MOUNT */
-            port = 20048;
-            break;
-        default:
-            chimera_nfs_error("rpcbind getaddr request for unknown program %u",
-                              args->r_prog);
-            addr.str = "";
-            addr.len = 0;
-            goto send_reply;
-    } /* switch */
+    portmap_getaddr_common(evpl, conn, args, msg,
+                           shared->portmap_v3.send_reply_rpcbproc_getaddr);
+} /* chimera_portmap_getaddr_v3 */
 
-    addr.len = portmap_make_uaddr(conn, port, uaddr, sizeof(uaddr));
-    addr.str = uaddr;
+/*
+ * Common implementation for V3/V4 DUMP.
+ * Returns list of all registered services.
+ */
+static void
+portmap_dump_common(
+    struct evpl           *evpl,
+    struct evpl_rpc2_conn *conn,
+    struct evpl_rpc2_msg  *msg,
+    int                  (*send_reply)(struct evpl *, struct rp__list *, void *))
+{
+    struct rp__list *list;
+    int              rc;
 
- send_reply:
-    rc = shared->portmap_v3.send_reply_rpcbproc_getaddr(evpl, &addr, msg);
+    list = portmap_build_rpcblist(conn, msg);
+
+    rc = send_reply(evpl, list, msg);
     chimera_nfs_abort_if(rc, "Failed to send RPC2 reply");
-} /* chimera_rpcbind_getaddr */
+}
 
 void
 chimera_portmap_dump_v3(
@@ -169,33 +300,10 @@ chimera_portmap_dump_v3(
 {
     struct chimera_server_nfs_thread *thread = private_data;
     struct chimera_server_nfs_shared *shared = thread->shared;
-    int                               rc;
-    struct rp__list                   rpcb_mountd, rpcb_nfsd;
-    char                              uaddr_mountd[64], uaddr_nfsd[64];
 
-    rpcb_mountd.rpcb_map.r_prog      = 100005;
-    rpcb_mountd.rpcb_map.r_vers      = 3;
-    rpcb_mountd.rpcb_map.r_netid.str = "tcp";
-    rpcb_mountd.rpcb_map.r_netid.len = 3;
-    rpcb_mountd.rpcb_map.r_addr.len  = portmap_make_uaddr(conn, 20048, uaddr_mountd, sizeof(uaddr_mountd));
-    rpcb_mountd.rpcb_map.r_addr.str  = uaddr_mountd;
-    rpcb_mountd.rpcb_map.r_owner.str = "";
-    rpcb_mountd.rpcb_map.r_owner.len = 0;
-    rpcb_mountd.next                 = &rpcb_nfsd;
-
-    rpcb_nfsd.rpcb_map.r_prog      = 100003;
-    rpcb_nfsd.rpcb_map.r_vers      = 3;
-    rpcb_nfsd.rpcb_map.r_netid.str = "tcp";
-    rpcb_nfsd.rpcb_map.r_netid.len = 3;
-    rpcb_nfsd.rpcb_map.r_addr.len  = portmap_make_uaddr(conn, 2049, uaddr_nfsd, sizeof(uaddr_nfsd));
-    rpcb_nfsd.rpcb_map.r_addr.str  = uaddr_nfsd;
-    rpcb_nfsd.rpcb_map.r_owner.str = "";
-    rpcb_nfsd.rpcb_map.r_owner.len = 0;
-    rpcb_nfsd.next                 = NULL;
-
-    rc = shared->portmap_v3.send_reply_rpcbproc_dump(evpl, &rpcb_mountd, msg);
-    chimera_nfs_abort_if(rc, "Failed to send RPC2 reply");
-} /* chimera_rpcbind_dump */
+    portmap_dump_common(evpl, conn, msg,
+                        shared->portmap_v3.send_reply_rpcbproc_dump);
+} /* chimera_portmap_dump_v3 */
 
 void
 chimera_portmap_dump_v4(
@@ -206,32 +314,9 @@ chimera_portmap_dump_v4(
 {
     struct chimera_server_nfs_thread *thread = private_data;
     struct chimera_server_nfs_shared *shared = thread->shared;
-    int                               rc;
-    struct rp__list                   rpcb_mountd, rpcb_nfsd;
-    char                              uaddr_mountd[64], uaddr_nfsd[64];
 
-    rpcb_mountd.rpcb_map.r_prog      = 100005;
-    rpcb_mountd.rpcb_map.r_vers      = 3;
-    rpcb_mountd.rpcb_map.r_netid.str = "tcp";
-    rpcb_mountd.rpcb_map.r_netid.len = 3;
-    rpcb_mountd.rpcb_map.r_addr.len  = portmap_make_uaddr(conn, 20048, uaddr_mountd, sizeof(uaddr_mountd));
-    rpcb_mountd.rpcb_map.r_addr.str  = uaddr_mountd;
-    rpcb_mountd.rpcb_map.r_owner.str = "";
-    rpcb_mountd.rpcb_map.r_owner.len = 0;
-    rpcb_mountd.next                 = &rpcb_nfsd;
-
-    rpcb_nfsd.rpcb_map.r_prog      = 100003;
-    rpcb_nfsd.rpcb_map.r_vers      = 3;
-    rpcb_nfsd.rpcb_map.r_netid.str = "tcp";
-    rpcb_nfsd.rpcb_map.r_netid.len = 3;
-    rpcb_nfsd.rpcb_map.r_addr.len  = portmap_make_uaddr(conn, 2049, uaddr_nfsd, sizeof(uaddr_nfsd));
-    rpcb_nfsd.rpcb_map.r_addr.str  = uaddr_nfsd;
-    rpcb_nfsd.rpcb_map.r_owner.str = "";
-    rpcb_nfsd.rpcb_map.r_owner.len = 0;
-    rpcb_nfsd.next                 = NULL;
-
-    rc = shared->portmap_v4.send_reply_RPCBPROC_DUMP(evpl, &rpcb_mountd, msg);
-    chimera_nfs_abort_if(rc, "Failed to send RPC2 reply");
+    portmap_dump_common(evpl, conn, msg,
+                        shared->portmap_v4.send_reply_RPCBPROC_DUMP);
 } /* chimera_portmap_dump_v4 */
 
 void
@@ -244,34 +329,7 @@ chimera_portmap_getaddr_v4(
 {
     struct chimera_server_nfs_thread *thread = private_data;
     struct chimera_server_nfs_shared *shared = thread->shared;
-    xdr_string                        addr;
-    char                              uaddr[64];
-    unsigned int                      port;
-    int                               rc;
 
-    /* Return universal address for known programs */
-    switch (args->r_prog) {
-        case 100000: /* PORTMAP */
-            port = 111;
-            break;
-        case 100003: /* NFS */
-            port = 2049;
-            break;
-        case 100005: /* MOUNT */
-            port = 20048;
-            break;
-        default:
-            chimera_nfs_error("rpcbind getaddr request for unknown program %u",
-                              args->r_prog);
-            addr.str = "";
-            addr.len = 0;
-            goto send_reply;
-    } /* switch */
-
-    addr.len = portmap_make_uaddr(conn, port, uaddr, sizeof(uaddr));
-    addr.str = uaddr;
-
- send_reply:
-    rc = shared->portmap_v4.send_reply_RPCBPROC_GETADDR(evpl, &addr, msg);
-    chimera_nfs_abort_if(rc, "Failed to send RPC2 reply");
+    portmap_getaddr_common(evpl, conn, args, msg,
+                           shared->portmap_v4.send_reply_RPCBPROC_GETADDR);
 } /* chimera_portmap_getaddr_v4 */
