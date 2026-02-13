8ec5604e313e36065ee5b1ea9f0e9f9e3f1c1ec2
Ben Jarvis <ben.jarvis@quantum.com>
2026-02-13 13:41:30 -0600

    Route Docker Hub pulls through registry proxy in CI (#188)
    * Route Docker Hub pulls through registry proxy in CI
    
    Configure all Docker image pulls to go through the Nexus pull-through
    proxy at repository.mdh.quantum.com:5001 when running in GitHub Actions,
    avoiding Docker Hub unauthenticated rate limits.
    
    * Update copyright years to include 2026

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SPDX-FileCopyrightText: 2025 Chimera-NAS Project Contributors
+# SPDX-FileCopyrightText: 2025-2026 Chimera-NAS Project Contributors
 #
 # SPDX-License-Identifier: Unlicense
 
@@ -20,4 +20,4 @@ if [ -z "$SOURCE" ]; then
     exit 1
 fi
 
-docker build --build-arg BUILD_TYPE=$1 -t chimera/chimera:$TAG $SOURCE
+docker build --build-arg BUILD_TYPE=$1 ${DOCKER_MIRROR:+--build-arg DOCKER_MIRROR=$DOCKER_MIRROR} -t chimera/chimera:$TAG $SOURCE
