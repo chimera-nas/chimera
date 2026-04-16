a24d9cbeba7c7a8cc1abf8e2f4b38be176750251
Alain Renaud <alain.renaud@quantum.com>
2026-04-16 15:47:42 +0000

    kvm: pass APT_MIRROR env var into docker run containers
    Workflow-level env vars are not automatically forwarded into docker run
    containers. DOCKER_MIRROR was explicitly passed with -e but APT_MIRROR
    was missing, so build_vm_image.sh never saw it. Add -e APT_MIRROR to
    the test and static-analysis docker run commands, and add APT_MIRROR
    support to build.sh.

--- build.sh
+++ build.sh
@@ -20,4 +20,7 @@ if [ -z "$SOURCE" ]; then
     exit 1
 fi
 
-docker build --build-arg BUILD_TYPE=$1 ${DOCKER_MIRROR:+--build-arg DOCKER_MIRROR=$DOCKER_MIRROR} -t chimera/chimera:$TAG $SOURCE
+docker build --build-arg BUILD_TYPE=$1 \
+    ${DOCKER_MIRROR:+--build-arg DOCKER_MIRROR=$DOCKER_MIRROR} \
+    ${APT_MIRROR:+--build-arg APT_MIRROR=$APT_MIRROR} \
+    -t chimera/chimera:$TAG $SOURCE
