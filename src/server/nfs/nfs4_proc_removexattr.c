37fc6f2a1b5f9b2560bd656b5467087c1a076b42
Ben Jarvis <ben.jarvis@quantum.com>
2026-05-25 07:47:49 -0500

    Add xattr support to VFS backends (#365)
    * Add xattr support to VFS backends
    
    * pynfs: enable xattr tests for VFS backends
    
    * style: format xattr backend changes
    
    * nfs4: use file handles for xattr operations

--- src/server/nfs/nfs4_proc_removexattr.c
+++ src/server/nfs/nfs4_proc_removexattr.c
@@ -76,7 +76,7 @@ chimera_nfs4_removexattr(
     chimera_vfs_open_fh(thread->vfs_thread, &req->cred,
                         req->fh,
                         req->fhlen,
-                        CHIMERA_VFS_OPEN_INFERRED | CHIMERA_VFS_OPEN_PATH,
+                        CHIMERA_VFS_OPEN_INFERRED,
                         chimera_nfs4_removexattr_open_callback,
                         req);
 } /* chimera_nfs4_removexattr */
