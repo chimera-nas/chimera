3fa3c85b94493b7d4a6fe6f36b2b188bc24de9aa
Ben Jarvis <bjarvismn@gmail.com>
2026-09-23 09:15:38 -0500

    Integrate Windows into the standard quick and extended CI tiers

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -35,8 +35,6 @@ else()
 endif()
 
 if(MSVC)
-    # Resolve native crash addresses from CI without requiring a debugger.
-    add_link_options(/MAP)
     add_compile_options(/W3 /WX $<$<COMPILE_LANGUAGE:C>:/experimental:c11atomics> /wd4244 /wd4267 /wd4018)
     add_compile_definitions(_CRT_SECURE_NO_WARNINGS _CRT_NONSTDC_NO_WARNINGS
                             WIN32_LEAN_AND_MEAN NOMINMAX)
